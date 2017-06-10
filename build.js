bbe4afdd34f9077a7bd4d9e33c2984aabb013399
Max Shvets <max.shv@gmail.com>
2017-06-10 13:26:31 -0400

    refactor build

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
-const { makeAdBlockClientFromFilePath } = require('ad-block/lib/util');
+const { AdBlockClient } = require('ad-block');
 const fs = require('fs');
 
-// build buffer
-makeAdBlockClientFromFilePath('./data/easylist.txt').then((c) => {
-  const buffer = c.serialize();
-  fs.writeFile('./data/buffer', buffer, function (err) {
-    if (err) return console.log(err);
-    console.log('easlist.txt buffer was saved');
-  });
+const filterRuleData = fs.readFileSync('./data/easylist.txt', 'utf8');
+const client = new AdBlockClient();
+client.parse(filterRuleData);
+const buffer = client.serialize();
+fs.writeFile('./data/buffer', buffer, function (err) {
+  if (err) return console.log(err);
+  console.log('easlist.txt buffer was saved');
 });
